6a5ee1a8ffd738121a19e52bff1a40e141848101
Julian Cobos <jujoco@users.noreply.github.com>
2024-03-28 05:32:06 -0700

    make 100 test coverage

--- test/Task.test.js
+++ test/Task.test.js
@@ -14,21 +14,41 @@ describe("Task", () => {
 		});
 	});
 
-	it("should initialize with the correct task description", () => {
-		expect(task.getDescription()).toBe("Buy groceries");
+	describe("getDescription", () => {
+		it("should return the description of the task", () => {
+			expect(task.getDescription()).toBe("Buy groceries");
+		});
 	});
 
-	it("should initialize with isComplete set to false", () => {
-		expect(task.completionStatus).toBe(false);
+	describe("setDescription", () => {
+		it("should set the description of the task", () => {
+			task.setDescription("Clean the house");
+			expect(task.getDescription()).toBe("Clean the house");
+		});
+
+		it("should return Error if description is not a string", () => {
+			expect(() => task.setDescription(123)).toThrow(
+				"Description must be a string"
+			);
+		});
 	});
 
-	it("should be able to set a new task description", () => {
-		task.setDescription("Clean the house");
-		expect(task.getDescription()).toBe("Clean the house");
+	describe("isComplete", () => {
+		it("should return the completion status of the task", () => {
+			expect(task.isComplete()).toBe(false);
+		});
 	});
 
-	it("should be able to set the task status to complete", () => {
-		task.setCompletionStatus(true);
-		expect(task.isComplete()).toBe(true);
+	describe("setCompletionStatus", () => {
+		it("should set the task status to complete", () => {
+			task.setCompletionStatus(true);
+			expect(task.isComplete()).toBe(true);
+		});
+
+		it("should return Error if status is not a boolean", () => {
+			expect(() => task.setCompletionStatus("true")).toThrow(
+				"Status must be a boolean"
+			);
+		});
 	});
 });
