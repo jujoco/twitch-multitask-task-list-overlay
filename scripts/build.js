4a83a2e3be6b73727a5e6b4df14cc3bd466f3f12
jujoco <jujocotm@gmail.com>
2024-03-28 13:38:55 -0700

    add build script

---
+++ scripts/build.js
@@ -0,0 +1,20 @@
+const fs = require("node:fs");
+
+const files = fs.readdirSync("src");
+console.log(files);
+
+let contents = "";
+
+files.forEach((file) => {
+	const fileContent = fs.readFileSync(`src/${file}`, "utf-8");
+	const lines = fileContent.split("\n");
+	const modifiedContent = lines
+		.filter(
+			(line) => !line.includes("require") && !line.includes("module.exports")
+		)
+		.join("\n");
+
+	contents += modifiedContent;
+});
+
+fs.writeFileSync("public/index.js", contents);
