c4449546af09e501617d77e5c439d2d54234d7d1
jujoco <jujocotm@gmail.com>
2024-03-27 19:11:38 -0700

    implement Task class + test

---
+++ test/Task.test.js
@@ -0,0 +1,28 @@
+const Task = require("../scripts/Task");
+
+describe("Task", () => {
+	/** @type Task */
+	let task;
+
+	beforeEach(() => {
+		task = new Task("Buy groceries");
+	});
+
+	it("should initialize with the correct task description", () => {
+		expect(task.getDescription()).toBe("Buy groceries");
+	});
+
+	it("should initialize with isComplete set to false", () => {
+		expect(task.completionStatus).toBe(false);
+	});
+
+	it("should be able to set a new task description", () => {
+		task.setDescription("Clean the house");
+		expect(task.getDescription()).toBe("Clean the house");
+	});
+
+	it("should be able to set the task status to complete", () => {
+		task.setCompletionStatus(true);
+		expect(task.isComplete()).toBe(true);
+	});
+});
