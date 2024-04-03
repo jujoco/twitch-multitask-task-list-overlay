a40e9d1f1ea16c8bf78595e26df294cbe5abe352
jujoco <jujocotm@gmail.com>
2024-04-02 23:27:13 -0700

    add test for User, Task, UserList to cover 100% functions

--- src/User.js
+++ src/User.js
@@ -4,13 +4,13 @@ const Task = require("./Task");
  * @class User
  * @property {string} username - The username of the user
  * @property {Task[]} tasks - The tasks of the user
+ * @method validateUsername - Add a task to the user
  * @method addTask - Add a task to the user
- * @method getTask - Get the task at the specified index
- * @method getTasks - Get the tasks of the user
- * @method updateTask - Update the task at the specified index
- * @method completeTask - Mark the task at the specified index as complete
+ * @method editTask - Edit the task at the specified index
+ * @method completeTask - Mark the task at index as complete
  * @method deleteTask - Delete the task at the specified index
- * @method getTaskCount - Get the number of tasks the user has
+ * @method getTask - Get the task at the specified index
+ * @method getTasks - Get all tasks of the user
  * @method validateTaskIndex - Validates the task index
  */
 class User {
@@ -19,18 +19,72 @@ class User {
 	 * @param {string} username - The username of the user
 	 */
 	constructor(username) {
-		this.username = username;
+		this.username = this.validateUsername(username);
 		this.tasks = [];
 	}
 
 	/**
-	 * Add a task to the user
-	 * @param {string} task - The task to add
-	 * @returns {void}
+	 * Validate the username of the user
+	 * @param {string} username - The username of the user
+	 * @returns {string} - The username of the user
+	 * @throws {Error} - If the username is invalid
 	 */
-	addTask(task) {
-		const newTask = new Task(task);
-		this.tasks.push(newTask);
+	validateUsername(username) {
+		if (typeof username !== "string" || username === "") {
+			throw new Error("Invalid username format");
+		}
+		return username;
+	}
+
+	/**
+	 * Add a task to the user
+	 * @param {string} description - The task to add
+	 * @returns {Task}
+	 */
+	addTask(description) {
+		const task = new Task(description);
+		this.tasks.push(task);
+		return task;
+	}
+
+	/**
+	 * Edit the task at the specified index
+	 * @param {number} index - The index of the task to get
+	 * @param {string} description - The new task description
+	 * @throws {Error} If the index is out of bounds
+	 * @returns {Task} - The task that was edited
+	 */
+	editTask(index, description) {
+		this.validateTaskIndex(index);
+		let task = this.getTask(index);
+		task.setDescription(description);
+		return task;
+	}
+
+	/**
+	 * Mark the task at the specified index as complete
+	 * @param {number} index - The index of the task to complete
+	 * @throws {Error} If the index is out of bounds
+	 * @returns {Task} - The task that was completed
+	 */
+	completeTask(index) {
+		this.validateTaskIndex(index);
+		let task = this.getTask(index);
+		task.setCompletionStatus(true);
+		return task;
+	}
+
+	/**
+	 * Delete the task at the specified index
+	 * @param {number} index - The index of the task to delete
+	 * @throws {Error} If the index is out of bounds
+	 * @returns {Task}	The task that was removed
+	 */
+	deleteTask(index) {
+		this.validateTaskIndex(index);
+		let tasks = this.getTasks();
+		const taskRemoved = tasks.splice(index, 1)[0];
+		return taskRemoved;
 	}
 
 	/**
@@ -53,59 +107,19 @@ class User {
 	}
 
 	/**
-	 * Update the task at the specified index
-	 * @param {number} index - The index of the task to get
-	 * @param {string} message - The new message for the task
-	 * @throws {Error} If the index is out of bounds
-	 * @returns {void}
-	 */
-	updateTask(index, message) {
-		this.validateTaskIndex(index);
-		let task = this.getTask(index);
-		task.setDescription(message);
-	}
-
-	/**
-	 * Mark the task at the specified index as complete
-	 * @param {number} index - The index of the task to complete
-	 * @throws {Error} If the index is out of bounds
-	 * @returns {void}
-	 */
-	completeTask(index) {
-		this.validateTaskIndex(index);
-		let task = this.getTask(index);
-		task.setCompletionStatus(true);
-	}
-
-	/**
-	 * Delete the task at the specified index
-	 * @param {number} index - The index of the task to delete
-	 * @throws {Error} If the index is out of bounds
-	 * @returns {void}
-	 */
-	deleteTask(index) {
-		this.validateTaskIndex(index);
-		let tasks = this.getTasks();
-		tasks.splice(index, 1);
-	}
-
-	/**
-	 * Get the count of tasks
-	 * @returns {number} The number of tasks the user has
-	 */
-	getTaskCount() {
-		return this.tasks.length;
-	}
-
-	/**
 	 * Validates the task index.
 	 * @param {number} index - The index of the task.
 	 * @throws {Error} If the index is out of bounds.
+	 * @returns {boolean}
 	 */
 	validateTaskIndex(index) {
-		if (index < 0 || index >= this.getTaskCount()) {
-			throw new Error("Task Index out of bounds");
+		if (typeof index !== "number" || isNaN(index)) {
+			throw new Error("Task index must be a number");
 		}
+		if (index < 0 || index >= this.tasks.length) {
+			throw new Error("Task index out of bounds");
+		}
+		return true;
 	}
 }
 module.exports = User;
--- src/UserList.js
+++ src/UserList.js
@@ -1,39 +1,113 @@
 const User = require("./User");
 
 /**
+ * @readonly
+ * @enum {string}
+ */
+const ActionType = {
+	ADD_USER_TASK: "ADD_USER_TASK",
+	EDIT_USER_TASK: "EDIT_USER_TASK",
+	COMPLETE_USER_TASK: "COMPLETE_USER_TASK",
+	DELETE_USER_TASK: "DELETE_USER_TASK",
+	CHECK_USER_TASKS: "CHECK_USER_TASKS",
+};
+
+/**
  * @class UserList
  * @property {User[]} users
- * @method addUser - Add a user to the list
+ * @method loadFromLocalStorage - Load the user list from local storage
+ * @method saveToLocalStorage - Save the user list to local storage
+ * @method dispatch - Dispatch an action to the user list
  * @method getUser - Get the user at the specified index
  * @method deleteUser - Delete the user at the specified index
- * @method getUserCount - Get the number of users in the list
- * @method addUserTask - Add a task to the user at the specified index
- * @method updateUserTask - Update the task at the specified index
- * @method deleteUserTask - Delete the task at the specified index
- * @method getUserTaskCount - Get the number of tasks the user has
+ * @method addUserTask - Add a task to the specified user
+ * @method editUserTask - Edit the task at the specified index
+ * @method completeUserTask - Mark specified task as complete
+ * @method deleteUserTask - Delete specified task at index
+ * @method checkUserTasks - Get inc from a user
  */
 class UserList {
 	/**
 	 * @constructor
 	 */
 	constructor() {
-		this.users = [];
+		this.users = this.loadFromLocalStorage();
 	}
 
 	/**
-	 * Add a user to the list
-	 * @param {string} username - The username of the user
+	 * Load the user list from local storage
+	 * @returns {User[]} - The user list
+	 */
+	loadFromLocalStorage() {
+		const usersStore = localStorage.getItem("userList");
+		const users = usersStore || "[]";
+		return JSON.parse(users);
+	}
+
+	/**
+	 * Save the user list to local storage
 	 * @returns {void}
 	 */
-	addUser(username) {
-		const newUser = new User(username);
-		this.users.push(newUser);
+	saveToLocalStorage() {
+		localStorage.setItem("userList", JSON.stringify(this.users));
+	}
+
+	/**
+	 * Dispatch an action to the user list
+	 * @typedef Response
+	 * @property {string[]} taskDescriptions - list of task descriptions
+	 * @property {string | null} error - The error message
+	 *
+	 * @param {ActionType} type - The action type
+	 * @param {{ username: string, taskIndex?: number, taskDescription?: string}} payload - The action payload
+	 * @returns {Response} - The response from the action
+	 */
+	dispatch(type, payload) {
+		const { username, taskDescription = "", taskIndex = -1 } = payload;
+		/** @type Response */
+		let response = {
+			taskDescriptions: [],
+			error: null,
+		};
+
+		try {
+			switch (type) {
+				case ActionType.ADD_USER_TASK:
+					response.taskDescriptions.push(
+						this.addUserTask(username, taskDescription)
+					);
+					break;
+				case ActionType.EDIT_USER_TASK:
+					response.taskDescriptions.push(
+						this.editUserTask(username, taskIndex, taskDescription)
+					);
+					break;
+				case ActionType.COMPLETE_USER_TASK:
+					response.taskDescriptions.push(
+						this.completeUserTask(username, taskIndex)
+					);
+					break;
+				case ActionType.DELETE_USER_TASK:
+					response.taskDescriptions.push(
+						this.deleteUserTask(username, taskIndex)
+					);
+					break;
+				case ActionType.CHECK_USER_TASKS:
+					response.taskDescriptions = this.checkUserTasks(username);
+					break;
+				default:
+					throw new Error("Invalid action type");
+			}
+		} catch (error) {
+			response.error = error.message;
+		}
+		return response;
 	}
 
 	/**
 	 * Get user by username
 	 * @param {string} username - The username of the user
-	 * @returns {User} - The user at the specified index
+	 * @returns {User | undefined} - The user at the specified index
 	 */
 	getUser(username) {
 		return this.users.find((user) => user.username === username);
@@ -42,73 +116,101 @@ class UserList {
 	/**
 	 * Delete a user by username
 	 * @param {string} username - The username of the user
-	 * @returns {void}
+	 * @throws {Error} - If the user is not found
+	 * @returns {User} - The deleted user
 	 */
 	deleteUser(username) {
 		const index = this.users.findIndex((user) => user.username === username);
-		this.users.splice(index, 1);
-	}
-
-	/**
-	 * Get the number of users in the list
-	 * @returns {number} - The number of users in the list
-	 */
-	getUserCount() {
-		return this.users.length;
+		if (index === -1) {
+			throw new Error("User not found");
+		}
+		const deletedUser = this.users.splice(index, 1)[0];
+		this.saveToLocalStorage();
+		return deletedUser;
 	}
 
 	/**
 	 * Add a task to the user at the specified index
 	 * @param {string} username - The username of the user
-	 * @param {string} task - The task to add
-	 * @returns {void}
+	 * @param {string} taskDescription - The task to add
+	 * @returns {string} - The description of the added task
 	 */
-	addUserTask(username, task) {
-		const user = this.getUser(username);
-		if (user) {
-			user.addTask(task);
-		}
+	addUserTask(username, taskDescription) {
+		const user = this.getUser(username) || new User(username);
+		user.addTask(taskDescription);
+		this.users.push(user);
+		this.saveToLocalStorage();
+		return taskDescription;
 	}
 
 	/**
-	 * Update the task at the specified index
+	 * Edit the task at the specified index
 	 * @param {string} username - The username of the user
-	 * @param {number} taskIndex - The index of the task to update
-	 * @param {string} newTask - The new task value
-	 * @returns {void}
+	 * @param {number} taskIndex - The index of the task to edit
+	 * @param {string} taskDescription - The new task value
+	 * @throws {Error} - If user has no tasks
+	 * @returns {string} - The new task description
 	 */
-	updateUserTask(username, taskIndex, newTask) {
+	editUserTask(username, taskIndex, taskDescription) {
 		const user = this.getUser(username);
-		if (user) {
-			user.updateTask(taskIndex, newTask);
+		if (!user) {
+			throw new Error(`${username} has no tasks`);
 		}
+		user.editTask(taskIndex, taskDescription);
+		this.saveToLocalStorage();
+		return taskDescription;
+	}
+
+	/**
+	 * Mark the task at the specified index as complete
+	 * @param {string} username - The username of the user
+	 * @param {number} taskIndex - The index of the task to complete
+	 * @throws {Error} - If user has no tasks
+	 * @returns {string} - The description of the completed task
+	 */
+	completeUserTask(username, taskIndex) {
+		const user = this.getUser(username);
+		if (!user) {
+			throw new Error(`${username} has no tasks`);
+		}
+		const task = user.completeTask(taskIndex);
+		this.saveToLocalStorage();
+		return task.getDescription();
 	}
 
 	/**
 	 * Delete the task at the specified index
 	 * @param {string} username - The username of the user
 	 * @param {number} taskIndex - The index of the task to delete
-	 * @returns {void}
+	 * @throws {Error} - If user has no tasks
+	 * @returns {string} - The description of the deleted task
 	 */
 	deleteUserTask(username, taskIndex) {
 		const user = this.getUser(username);
-		if (user) {
-			user.deleteTask(taskIndex);
+		if (!user) {
+			throw new Error(`${username} has no tasks`);
 		}
+		const task = user.deleteTask(taskIndex);
+		this.saveToLocalStorage();
+		return task.getDescription();
 	}
 
 	/**
-	 * Get the number of tasks the user has
+	 * Get incomplete tasks from a user
 	 * @param {string} username - The username of the user
-	 * @returns {number} - The number of tasks the user has
+	 * @throws {Error} - If the user has no tasks
+	 * @returns {string[]} - The incomplete tasks of the user
 	 */
-	getUserTaskCount(username) {
+	checkUserTasks(username) {
 		const user = this.getUser(username);
-		if (user) {
-			return user.getTaskCount();
+		if (!user) {
+			throw new Error(`${username} has no tasks`);
 		}
-		return 0;
+		return user
+			.getTasks()
+			.filter((task) => !task.isComplete())
+			.map((task) => task.getDescription());
 	}
 }
 
-module.exports = UserList;
+module.exports = { UserList, ActionType };
