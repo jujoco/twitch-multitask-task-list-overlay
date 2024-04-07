36780e64316e916a5fd5527584aad9824d690c15
jujoco <jujocotm@gmail.com>
2024-04-06 20:57:06 -0700

    switch to 'else if' conditions

--- src/comfyExec.js
+++ src/comfyExec.js
@@ -39,55 +39,52 @@ ComfyJS.onCommand = (username, command, message, flags, extra) => {
 		// USER COMMANDS
 		if (userConfig.commands.addTask.includes(command)) {
 			// ADD TASK
-			if (userList.getUser(username).tasks.length >= maxTasksPerUser) {
-				respond(userConfig.responseTo[langCode].maxTasksReached, username);
+			if (userList.getUser(username)?.tasks.length >= maxTasksPerUser) {
+				respond(userConfig.responseTo[langCode].maxTasksAdded, username);
+			} else {
+				const tasks = message.split(", ");
+				userList.addUserTask(username, tasks);
+				respond(userConfig.responseTo[langCode].addTask, username, message);
 			}
-			const tasks = message.split(", ");
-			userList.addUserTask(username, tasks);
-			respond(userConfig.responseTo[langCode].addTask, username, message);
-			return renderTaskBot();
-		}
-		if (userConfig.commands.editTask.includes(command)) {
+		} else if (userConfig.commands.editTask.includes(command)) {
 			// EDIT TASK
-			const [strIndex, newTask] = message.split(", ");
-			const index = parseIndex(strIndex);
-			userList.editUserTask(username, index, newTask);
-			respond(userConfig.responseTo[langCode].editTask, username, newTask);
-			return renderTaskBot();
-		}
-		if (userConfig.commands.finishTask.includes(command)) {
+			const regex = /(?<=\d)\s/;
+			const whiteSpaceIndex = message.search(regex);
+			const taskDescription = message.slice(whiteSpaceIndex + 1);
+			const taskNumber = parseIndex(message.slice(0, whiteSpaceIndex));
+			userList.editUserTask(username, taskNumber, taskDescription);
+			respond(
+				userConfig.responseTo[langCode].editTask,
+				username,
+				taskDescription
+			);
+		} else if (userConfig.commands.finishTask.includes(command)) {
 			// COMPLETE TASK
 			const index = parseIndex(message);
 			const taskComp = userList.completeUserTask(username, index);
 			respond(userConfig.responseTo[langCode].finishTask, username, taskComp);
-			return renderTaskBot();
-		}
-		if (userConfig.commands.deleteTask.includes(command)) {
+		} else if (userConfig.commands.deleteTask.includes(command)) {
 			// DELETE TASK
 			const index = parseIndex(message);
 			const taskDel = userList.deleteUserTask(username, index);
 			respond(userConfig.responseTo[langCode].deleteTask, username, taskDel);
-			return renderTaskBot();
-		}
-		if (userConfig.commands.check.includes(command)) {
+		} else if (userConfig.commands.check.includes(command)) {
 			// CHECK TASKS
 			const tasks = userList.checkUserTasks(username).join(", ");
 			respond(userConfig.responseTo[langCode].check, username, tasks);
-			return renderTaskBot();
-		}
-		if (userConfig.commands.help.includes(command)) {
+		} else if (userConfig.commands.help.includes(command)) {
 			// HELP COMMAND
 			respond(userConfig.responseTo[langCode].help);
-			return renderTaskBot();
-		}
-		if (userConfig.commands.additional[command]) {
+		} else if (userConfig.commands.additional.includes(command)) {
 			// ADDITIONAL COMMANDS
 			respond(userConfig.responseTo[langCode].additional);
-			return renderTaskBot();
+		} else {
+			throw new Error("Invalid command");
 		}
+		return renderTaskListToDOM();
 	} catch (error) {
-		console.log(error, username, message);
-		respond(error.message, user, message);
+		console.error(error, username, message);
+		respond(userConfig.responseTo[langCode].invalidCommand, username);
 	}
 };
 
