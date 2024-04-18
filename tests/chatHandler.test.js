b15d9b7d938164196d6f4045ab6437221b4dae34
Julian Cobos <jujoco@users.noreply.github.com>
2024-04-18 01:09:46 -0700

    add chatHandler test & test globalSetup

---
+++ tests/chatHandler.test.js
@@ -0,0 +1,302 @@
+import { beforeEach, describe, expect, it, test } from "vitest";
+import { chatHandler } from "../src/chatHandler";
+
+describe("chatHandler", () => {
+	/** @typedef {import('../src/classes/UserList').default} UserList */
+	/** @type UserList */
+	const userList = window.userList;
+	const adminUser = {
+		username: "bobTheAdmin",
+		flags: {
+			broadcaster: true,
+			mod: false,
+		},
+		extra: { color: "#FF0000" },
+		command: {
+			CLEARLIST: "clearList",
+			CLEARDONE: "clearDone",
+			CLEARUSER: "clearUser",
+		},
+	};
+	const chatUser = {
+		username: "joeTheUser",
+		flags: {
+			broadcaster: false,
+			mod: false,
+		},
+		extra: { color: "#00FFFF" },
+		command: {
+			ADDTASK: "addTask",
+			EDITTASK: "editTask",
+			DONETASK: "doneTask",
+			DELETETASK: "deleteTask",
+			CHECKTASK: "checkTask",
+			HELP: "help",
+			ADDITIONAL: "credit",
+		},
+	};
+
+	beforeEach(() => {
+		userList.clearUserList();
+		userList.createUser("joeTheUser", { nameColor: "#00FFFF" });
+		userList.addUserTasks("joeTheUser", ["task1", "task2"]);
+		userList.completeUserTasks("joeTheUser", 0);
+
+		userList.createUser("bobTheAdmin", { nameColor: "#FF0000" });
+		userList.addUserTasks("bobTheAdmin", ["task1", "task2"]);
+	});
+
+	describe("Admin commands", () => {
+		test("!clearList ", () => {
+			const response = chatHandler(
+				adminUser.username,
+				adminUser.command.CLEARLIST,
+				"",
+				adminUser.flags,
+				adminUser.extra
+			);
+			expect(userList.users.length).toBe(0);
+			expect(response).toBe("bobTheAdmin, All tasks have been cleared");
+		});
+
+		test("!clearDone", () => {
+			const response = chatHandler(
+				adminUser.username,
+				adminUser.command.CLEARDONE,
+				"",
+				adminUser.flags,
+				adminUser.extra
+			);
+			expect(userList.users[0].getTasks().length).toBe(1);
+			expect(response).toBe(
+				"bobTheAdmin, All done tasks have been cleared"
+			);
+		});
+
+		test("!clearUser", () => {
+			const response = chatHandler(
+				adminUser.username,
+				adminUser.command.CLEARUSER,
+				chatUser.username,
+				adminUser.flags,
+				adminUser.extra
+			);
+			expect(userList.users.length).toBe(1);
+			expect(response).toBe(
+				"bobTheAdmin, All tasks for joeTheUser have been cleared"
+			);
+		});
+	});
+
+	describe("User commands", () => {
+		describe("!addTask", () => {
+			it("should return a success message showing the added tasks", () => {
+				const response = chatHandler(
+					chatUser.username,
+					chatUser.command.ADDTASK,
+					"newTask",
+					chatUser.flags,
+					chatUser.extra
+				);
+				expect(userList.users[0].getTasks().length).toBe(3);
+				expect(response).toBe('Task(s) "newTask" added, joeTheUser!');
+			});
+
+			it("should accept multiple, comma separated, tasks", () => {
+				const response = chatHandler(
+					chatUser.username,
+					chatUser.command.ADDTASK,
+					"newTask, newTask2",
+					chatUser.flags,
+					chatUser.extra
+				);
+				expect(userList.users[0].getTasks().length).toBe(4);
+				expect(response).toBe(
+					'Task(s) "newTask, newTask2" added, joeTheUser!'
+				);
+			});
+
+			it("should return error message if the user over assigns tasks", () => {
+				const response = chatHandler(
+					chatUser.username,
+					chatUser.command.ADDTASK,
+					"newTask, newTask2, newTask3, newTask4, newTask5",
+					chatUser.flags,
+					chatUser.extra
+				);
+				expect(userList.users[0].getTasks().length).toBe(2);
+				expect(response).toBe(
+					"joeTheUser, maximum number of tasks reached, try removing some first."
+				);
+			});
+		});
+
+		describe("!editTask", () => {
+			it("should return a message indicating the edited task number", () => {
+				const response = chatHandler(
+					chatUser.username,
+					chatUser.command.EDITTASK,
+					"2 editedTask",
+					chatUser.flags,
+					chatUser.extra
+				);
+				expect(
+					userList
+						.getUser(chatUser.username)
+						.getTask(1)
+						.getDescription()
+				).toBe("editedTask");
+				expect(response).toBe('Task "2" updated, joeTheUser!');
+			});
+
+			it("should return an error message if the message is empty", () => {
+				const response = chatHandler(
+					chatUser.username,
+					chatUser.command.EDITTASK,
+					"",
+					chatUser.flags,
+					chatUser.extra
+				);
+				expect(response).toBe(
+					'joeTheUser, Invalid command: "Task number or description format is invalid" - Try !help'
+				);
+			});
+
+			it("should return an error message if the task number is missing", () => {
+				const response = chatHandler(
+					chatUser.username,
+					chatUser.command.EDITTASK,
+					"edited Task",
+					chatUser.flags,
+					chatUser.extra
+				);
+				expect(response).toBe(
+					'joeTheUser, Invalid command: "Task number or description format is invalid" - Try !help'
+				);
+			});
+
+			it("should return an error message if task description is missing", () => {
+				const response = chatHandler(
+					chatUser.username,
+					chatUser.command.EDITTASK,
+					"2",
+					chatUser.flags,
+					chatUser.extra
+				);
+				expect(response).toBe(
+					'joeTheUser, Invalid command: "Task number or description format is invalid" - Try !help'
+				);
+			});
+		});
+
+		describe("!doneTask", () => {
+			it("should return a success message when marking task as done", () => {
+				const response = chatHandler(
+					chatUser.username,
+					chatUser.command.DONETASK,
+					"1",
+					chatUser.flags,
+					chatUser.extra
+				);
+				expect(
+					userList
+						.getUser(chatUser.username)
+						.getTasks()[0]
+						.isComplete()
+				).toBe(true);
+				expect(response).toBe(
+					'Good job on finishing "task1" joeTheUser!'
+				);
+			});
+
+			it("should return an error message if the task is not found", () => {
+				const response = chatHandler(
+					chatUser.username,
+					chatUser.command.DONETASK,
+					"3",
+					chatUser.flags,
+					chatUser.extra
+				);
+				expect(userList.users[0].getTasks().length).toBe(2);
+				expect(response).toBe(
+					'joeTheUser, Invalid command: "Task index out of bounds" - Try !help'
+				);
+			});
+		});
+
+		describe("!deleteTask", () => {
+			it("should return a message indicating the task has been delete", () => {
+				const response = chatHandler(
+					chatUser.username,
+					chatUser.command.DELETETASK,
+					"1",
+					chatUser.flags,
+					chatUser.extra
+				);
+				expect(userList.users[0].getTasks().length).toBe(1);
+				expect(response).toBe(
+					'Task "task1" has been deleted, joeTheUser!'
+				);
+			});
+
+			it("should return an error message if the task is not found", () => {
+				const response = chatHandler(
+					chatUser.username,
+					chatUser.command.DELETETASK,
+					"3",
+					chatUser.flags,
+					chatUser.extra
+				);
+				expect(userList.users[0].getTasks().length).toBe(2);
+				expect(response).toBe(
+					'joeTheUser, Invalid command: "Task index out of bounds" - Try !help'
+				);
+			});
+		});
+
+		describe("!checkTask", () => {
+			it("!checkTask", () => {
+				const response = chatHandler(
+					chatUser.username,
+					chatUser.command.CHECKTASK,
+					"",
+					chatUser.flags,
+					chatUser.extra
+				);
+				expect(response).toBe(
+					'Your current task is: "task2", joeTheUser'
+				);
+			});
+		});
+
+		describe("!help", () => {
+			it("!help", () => {
+				const response = chatHandler(
+					chatUser.username,
+					chatUser.command.HELP,
+					"",
+					chatUser.flags,
+					chatUser.extra
+				);
+				expect(response).toBe(
+					"Try using these commands - !add !edit !done !delete"
+				);
+			});
+		});
+
+		describe("!credit", () => {
+			it("!credit", () => {
+				const response = chatHandler(
+					chatUser.username,
+					chatUser.command.ADDITIONAL,
+					"",
+					chatUser.flags,
+					chatUser.extra
+				);
+				expect(response).toBe(
+					"Jujoco is the creator of this bot, check out his Twitch at: https://www.twitch.tv/JujocoCS"
+				);
+			});
+		});
+	});
+});
---
+++ tests/globalSetup.js
@@ -0,0 +1,58 @@
+import { vi } from "vitest";
+import UserList from "../src/classes/UserList";
+
+const configs = {
+	admin: {
+		commands: {
+			clearList: ["!clearlist"],
+			clearDone: ["!cleardone"],
+			clearUser: ["!clearuser"],
+		},
+		responseTo: {
+			EN: {
+				clearList: "{user}, All tasks have been cleared",
+				clearDone: "{user}, All done tasks have been cleared",
+				clearUser: "{user}, All tasks for {message} have been cleared",
+			},
+		},
+	},
+	user: {
+		commands: {
+			addTask: ["!add", "!task", "!addtask", "!taskadd"],
+			editTask: ["!edit", "!edittask", "!taskedit"],
+			finishTask: ["!done", "!donetask", "!taskdone"],
+			deleteTask: ["!delete", "!deletetask", "!taskdelete"],
+			check: ["!check", "!taskcheck", "!checktask", "!mytask"],
+			help: ["!help", "!taskhelp", "!helptask"],
+			additional: ["!credit", "!taskbot"],
+		},
+		responseTo: {
+			EN: {
+				addTask: 'Task(s) "{message}" added, {user}!',
+				editTask: 'Task "{message}" updated, {user}!',
+				finishTask: 'Good job on finishing "{message}" {user}!',
+				deleteTask: 'Task "{message}" has been deleted, {user}!',
+				check: 'Your current task is: "{message}", {user}',
+				help: "Try using these commands - !add !edit !done !delete",
+				additional:
+					"Jujoco is the creator of this bot, check out his Twitch at: https://www.twitch.tv/JujocoCS",
+				maxTasksAdded:
+					"{user}, maximum number of tasks reached, try removing some first.",
+				noTaskFound: "Looks like you don't have a task up there {user}",
+				invalidCommand:
+					'{user}, Invalid command: "{message}" - Try !help',
+			},
+		},
+	},
+	settings: {
+		languageCode: "EN", // "EN", "ES", "FR", "JP", etc.
+		maxTasksPerUser: 5, // default 5
+		scrollSpeed: 50, // default 50
+		crossTasksOnDone: true, // true or false
+		showUsernameColor: true, // true or false
+		testMode: false, // true or false - for testing purposes
+	},
+};
+
+vi.stubGlobal("configs", configs);
+vi.stubGlobal("userList", new UserList());
